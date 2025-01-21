4d0a4700d6a773114b592988c92a7cf9f2baeaed
kkrishhh <soham6218@gmail.com>
2025-01-21 23:54:33 +0530

    added

--- january/pattern.java
+++ january/pattern.java
@@ -4,5 +4,9 @@ public class pattern {
         System.out.println("***");
         System.out.println("**");
         System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+
     }
 }
